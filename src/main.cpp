041c4238c2d26dea337057ad25ee559f713fe5f1
drew65 <drew.scott65@gmail.com>
2017-08-12 14:38:33 +0100

    Update to working PID controller

--- src/main.cpp
+++ src/main.cpp
@@ -31,9 +31,12 @@ std::string hasData(std::string s) {
 int main()
 {
   uWS::Hub h;
-
+  double kp = 0.2;
+  double ki = 0.004;
+  double kd = 3.0;
   PID pid;
   // TODO: Initialize the pid variable.
+  pid.Init(kp, ki, kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,9 +60,12 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+          std::cout << "CTE: " << cte << "Angle " << angle << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
